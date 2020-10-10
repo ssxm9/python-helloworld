8d59c20147e737a9d5bde366e5a0fd532639a94f
Satyabrata Mishra <45105711+ssxm9@users.noreply.github.com>
2020-10-10 21:20:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
   backend "gcs" {   
     bucket  = "terraform-remote-state-test1"
     prefix = "${var.env}"
-    path    = "${var.env}/terraform.tfstate"   
+    path    = "/terraform.tfstate"   
 #    project = "<YOUR PROJECT ID>" 
   }
 }
