71a674f3416bf0cd634a8dbd717143c52cf8a991
Satyabrata Mishra <45105711+ssxm9@users.noreply.github.com>
2020-11-23 01:12:30 +0530

    Update bash-dags.py

--- dags/bash-dags.py
+++ dags/bash-dags.py
@@ -1,4 +1,4 @@
-from airflow import DAG 
+from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
