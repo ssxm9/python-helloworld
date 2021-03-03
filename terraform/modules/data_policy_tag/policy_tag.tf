f4f066e50f90fd51f0a8c2ec9b220e3bf09fd020
Satyabrata Mishra <45105711+ssxm9@users.noreply.github.com>
2021-03-03 19:23:58 +0530

    Update policy_tag.tf

--- terraform/modules/data_policy_tag/policy_tag.tf
+++ terraform/modules/data_policy_tag/policy_tag.tf
@@ -15,7 +15,7 @@ resource "google_data_catalog_policy_tag" "basic_policy_tag_low" {
 resource "google_data_catalog_taxonomy" "my_taxonomy" {
   provider               = google-beta
   region                 = "us"
-  display_name           = "test_tx"
+  display_name           = "test_tx1"
   description            = "A collection of policy tags"
   activated_policy_types = ["FINE_GRAINED_ACCESS_CONTROL"]
 }
