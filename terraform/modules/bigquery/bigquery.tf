e034dd53ba5ff9f9a177b1d45e5114750f5fadf2
sxm9 <satyabrata@sigmoidanalytics.com>
2021-03-03 19:43:58 +0530

    update

--- terraform/modules/bigquery/bigquery.tf
+++ terraform/modules/bigquery/bigquery.tf
@@ -26,47 +26,47 @@ resource "google_bigquery_table" "table_bq" {
     env = "dev"
   }
 
-#  schema = file("schemas/test.json")
-  schema = <<EOF
-    [
-      {
-        "description": "Full visitor ID",
-        "mode": "NULLABLE",
-        "name": "fullVisitorId",
-        "type": "STRING",
-        "policyTags": {
-          "names": ["${module.policy_tag.google_data_catalog_policy_tag_high}"]
-        }
-      },
-      {
-        "description": "Visit number",
-        "mode": "NULLABLE",
-        "name": "visitNumber",
-        "type": "INTEGER",
-        "policyTags": {
-          "names": ["${module.policy_tag.google_data_catalog_policy_tag_low}"]
-        }
-      },
-      {
-        "description": "Visit ID",
-        "mode": "NULLABLE",
-        "name": "visitId",
-        "type": "INTEGER"
-      },
-      {
-        "description": "Visit Start Time",
-        "mode": "NULLABLE",
-        "name": "visitStartTime",
-        "type": "INTEGER"
-      },
-      {
-        "description": "Full Date of Visit",
-        "mode": "NULLABLE",
-        "name": "fullDate",
-        "type": "DATE"
-      }
-   ]
-EOF    
+  schema = file("schemas/test.json")
+#  schema = <<EOF
+#    [
+#      {
+#        "description": "Full visitor ID",
+#        "mode": "NULLABLE",
+#        "name": "fullVisitorId",
+#        "type": "STRING",
+#        "policyTags": {
+#          "names": ["${module.policy_tag.google_data_catalog_policy_tag_high}"]
+#        }
+#      },
+#      {
+#        "description": "Visit number",
+#        "mode": "NULLABLE",
+#        "name": "visitNumber",
+#        "type": "INTEGER",
+#        "policyTags": {
+#          "names": ["${module.policy_tag.google_data_catalog_policy_tag_low}"]
+#        }
+#      },
+#      {
+#        "description": "Visit ID",
+#        "mode": "NULLABLE",
+#        "name": "visitId",
+#        "type": "INTEGER"
+#      },
+#      {
+#        "description": "Visit Start Time",
+#        "mode": "NULLABLE",
+#        "name": "visitStartTime",
+#        "type": "INTEGER"
+#      },
+#      {
+#        "description": "Full Date of Visit",
+#        "mode": "NULLABLE",
+#        "name": "fullDate",
+#        "type": "DATE"
+#      }
+#   ]
+#EOF    
 
   #  lifecycle {
   #        ignore_changes = [dataset_id,schema]
