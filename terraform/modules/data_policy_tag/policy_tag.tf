dc95589ee83995d7443face0ec58eeee1ca36c24
sxm9 <satyabrata@sigmoidanalytics.com>
2021-03-04 09:15:32 +0530

    update

--- terraform/modules/data_policy_tag/policy_tag.tf
+++ terraform/modules/data_policy_tag/policy_tag.tf
@@ -15,18 +15,27 @@ resource "google_data_catalog_policy_tag" "basic_policy_tag_low" {
 resource "google_data_catalog_taxonomy" "my_taxonomy" {
   provider               = google-beta
   region                 = "us"
-  display_name           = "txt_tx123"
+  display_name           = "txt_-tx"
   description            = "A collection of policy tags"
   activated_policy_types = ["FINE_GRAINED_ACCESS_CONTROL"]
 }
 
-resource "google_data_catalog_policy_tag_iam_member" "member" {
+#resource "google_data_catalog_policy_tag_iam_member" "member" {
+#  provider   = google-beta
+#  policy_tag = google_data_catalog_policy_tag.basic_policy_tag_high.name
+#  role       = "roles/datacatalog.categoryFineGrainedReader"
+#  member     = "user:satyabratasiliconindia@gmail.com"
+#}
+
+resource "google_data_catalog_policy_tag_iam_binding" "binding" {
   provider   = google-beta
   policy_tag = google_data_catalog_policy_tag.basic_policy_tag_high.name
   role       = "roles/datacatalog.categoryFineGrainedReader"
-  member     = "user:satyabratasiliconindia@gmail.com"
+  members = [
+    "user:satyabratamishra509@gmail.com",
+    "user:satyabratasiliconindia@gmail.com"
+  ]
 }
-
 output "google_data_catalog_policy_tag_high" {
   value = google_data_catalog_policy_tag.basic_policy_tag_high.id
 }
