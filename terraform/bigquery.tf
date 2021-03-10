fc5ad157e0bff1ab7a4ad4a95ee0d08f3b65adb3
Satyabrata Mishra <45105711+ssxm9@users.noreply.github.com>
2021-03-10 17:57:24 +0530

    Update bigquery.tf

--- terraform/bigquery.tf
+++ terraform/bigquery.tf
@@ -1,40 +1,40 @@
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id    = "example_dataset" 
-  friendly_name = "test"
-  description   = "This is a test description"
-  location      = "US" 
-
-  labels = {
-    env = var.env
-  }
-}
-
-resource "google_bigquery_table" "table" {
-  dataset_id = google_bigquery_dataset.dataset.dataset_id
-  table_id   = "bq_table" 
-
-  time_partitioning {
-    type = "DAY"
-  }
-
-  labels = {
-    env = var.env
-  }
-
-  schema = <<EOF
-[
-  {
-    "name": "permalink",
-    "type": "STRING",
-    "mode": "NULLABLE",
-    "description": "The Permalink"
-  },
-  {
-    "name": "state",
-    "type": "STRING",
-    "mode": "NULLABLE",
-    "description": "State where the head office is located"
-  }
-]
-EOF
-}
+#resource "google_bigquery_dataset" "dataset" {
+#  dataset_id    = "example_dataset" 
+#  friendly_name = "test"
+#  description   = "This is a test description"
+#  location      = "US" 
+#
+#  labels = {
+#    env = var.env
+#  }
+#}
+#
+#resource "google_bigquery_table" "table" {
+#  dataset_id = google_bigquery_dataset.dataset.dataset_id
+#  table_id   = "bq_table" 
+#
+#  time_partitioning {
+#    type = "DAY"
+#  }
+#
+#  labels = {
+#    env = var.env
+#  }
+#
+#  schema = <<EOF
+#[
+#  {
+#    "name": "permalink",
+#    "type": "STRING",
+#    "mode": "NULLABLE",
+#    "description": "The Permalink"
+#  },
+#  {
+#    "name": "state",
+#    "type": "STRING",
+#    "mode": "NULLABLE",
+#    "description": "State where the head office is located"
+#  }
+#]
+#EOF
+#}
